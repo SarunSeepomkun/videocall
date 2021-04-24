4f32e7b602b02c67dd6566e328222de92b827945
Sarun Seepomkun <sarunseepomkun@gmail.com>
2021-04-25 00:06:58 +0700

    First Version
    First Version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Peer from "simple-peer";
+import io from "socket.io-client";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
+const socket = io.connect("https://ipeach-videocall.herokuapp.com/");
 function App() {
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded , setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+  useEffect(()=>{
+    
+    navigator.mediaDevices.getUserMedia({video:true , audio:true}).then((stream)=>{
+      setStream(stream);
+      myVideo.current.srcObject = stream;
+    });
+
+    socket.on("me",(id)=>{
+      setMe(id);
+    });
+
+    socket.on("callUser",(data)=>{
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+
+  },[]);
+
+  const callUser = (id)=>{
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream
+    });
+    peer.on("signal",(data)=>{
+      socket.emit("callUser",{
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name
+      });
+    });
+    peer.on("stream",(stream)=>{
+      userVideo.current.srcObject = stream;
+    });
+    socket.on("callAccepted",(signal)=>{
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+    connectionRef.current = peer;
+  }
+
+  const answerCall = ()=>{
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream
+    });
+    peer.on("signal",(data)=>{
+      socket.emit("answerCall",{ signal:data , to:caller });
+    });
+    peer.on("stream",(stream)=>{
+      userVideo.current.srcObject = stream;
+    });
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  }
+
+  const leaveCall = ()=>{
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div className="container">
+      <div className="video-container">
+        <div className="video">
+          {stream && <video playsInline muted ref={myVideo} autoPlay style={{width:"300px"}} />}
+        </div>
+        <div className="video">
+          {callAccepted && !callEnded ? <video playsInline ref={userVideo} autoPlay style={{width:"300px"}} /> : null}
+        </div>
+      </div>
+      <div className="myId">
+        Name : <input type="text" id="filled-basic" value={name} onChange={(e)=> setName(e.target.value)} />
+        <CopyToClipboard text={me}>
+          <button>Copy ID</button>
+        </CopyToClipboard>
+        Call to : <input type="text" value={idToCall} onChange={(e)=> setIdToCall(e.target.value)} />
+        <div className="call-button">
+          {callAccepted && ! callEnded ? (<button onClick={leaveCall}>End Call</button>)
+          :
+          (<button onClick={()=> callUser(idToCall)}>Call</button>)}
+          {idToCall}
+        </div>
+      </div>
+      <div>
+        {receivingCall && !callAccepted ? (
+        <div className="caller">
+          <h1>{name} is Calling...</h1>
+          <button onClick={answerCall}>Answer</button>
+        </div>
+        ) : null}
+      </div>
     </div>
+  </div>
   );
 }
 
