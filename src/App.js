58f9f3a517b08dbc9948c26ca93f3594e0a75068
Sarun Seepomkun <sarunseepomkun@gmail.com>
2021-04-26 23:58:28 +0700

    Update design
    Update design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Peer from "simple-peer";
 import io from "socket.io-client";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Grid, Switch, Button, TextField, Paper } from "@material-ui/core";
 
 const socket = io.connect("https://ipeach-videocall.herokuapp.com/");
 function App() {
@@ -13,112 +14,189 @@ function App() {
   const [callerSignal, setCallerSignal] = useState();
   const [callAccepted, setCallAccepted] = useState(false);
   const [idToCall, setIdToCall] = useState("");
-  const [callEnded , setCallEnded] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
   const [name, setName] = useState("");
+  const [turnCamera, setTurnCarema] = useState(true);
   const myVideo = useRef();
   const userVideo = useRef();
   const connectionRef = useRef();
 
-  useEffect(()=>{
-    
-    navigator.mediaDevices.getUserMedia({video:true , audio:true}).then((stream)=>{
-      setStream(stream);
-      myVideo.current.srcObject = stream;
-    });
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: turnCamera, audio: true })
+      .then((stream) => {
+        setStream(stream);
+        myVideo.current.srcObject = stream;
+      });
 
-    socket.on("me",(id)=>{
+    socket.on("me", (id) => {
       setMe(id);
     });
 
-    socket.on("callUser",(data)=>{
+    socket.on("callUser", (data) => {
       setReceivingCall(true);
       setCaller(data.from);
       setName(data.name);
       setCallerSignal(data.signal);
     });
+  }, [turnCamera]);
 
-  },[]);
-
-  const callUser = (id)=>{
+  const callUser = (id) => {
     const peer = new Peer({
       initiator: true,
       trickle: false,
-      stream: stream
+      stream: stream,
     });
-    peer.on("signal",(data)=>{
-      socket.emit("callUser",{
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
         userToCall: id,
         signalData: data,
         from: me,
-        name: name
+        name: name,
       });
     });
-    peer.on("stream",(stream)=>{
+    peer.on("stream", (stream) => {
       userVideo.current.srcObject = stream;
     });
-    socket.on("callAccepted",(signal)=>{
+    socket.on("callAccepted", (signal) => {
       setCallAccepted(true);
       peer.signal(signal);
     });
     connectionRef.current = peer;
-  }
+  };
 
-  const answerCall = ()=>{
+  const answerCall = () => {
     setCallAccepted(true);
     const peer = new Peer({
       initiator: false,
       trickle: false,
-      stream: stream
+      stream: stream,
     });
-    peer.on("signal",(data)=>{
-      socket.emit("answerCall",{ signal:data , to:caller });
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: caller });
     });
-    peer.on("stream",(stream)=>{
+    peer.on("stream", (stream) => {
       userVideo.current.srcObject = stream;
     });
     peer.signal(callerSignal);
     connectionRef.current = peer;
-  }
+  };
 
-  const leaveCall = ()=>{
+  const leaveCall = () => {
     setCallEnded(true);
     connectionRef.current.destroy();
-  }
+  };
+
+  const checkTurnCamera = (e) => {
+    setTurnCarema(e.target.checked);
+  };
 
   return (
-  <div className="App">
-    <div className="container">
-      <div className="video-container">
-        <div className="video">
-          {stream && <video playsInline muted ref={myVideo} autoPlay style={{width:"300px"}} />}
-        </div>
-        <div className="video">
-          {callAccepted && !callEnded ? <video playsInline ref={userVideo} autoPlay style={{width:"300px"}} /> : null}
-        </div>
-      </div>
-      <div className="myId">
-        Name : <input type="text" id="filled-basic" value={name} onChange={(e)=> setName(e.target.value)} />
-        <CopyToClipboard text={me}>
-          <button>Copy ID</button>
-        </CopyToClipboard>
-        Call to : <input type="text" value={idToCall} onChange={(e)=> setIdToCall(e.target.value)} />
-        <div className="call-button">
-          {callAccepted && ! callEnded ? (<button onClick={leaveCall}>End Call</button>)
-          :
-          (<button onClick={()=> callUser(idToCall)}>Call</button>)}
-          {idToCall}
-        </div>
-      </div>
-      <div>
-        {receivingCall && !callAccepted ? (
-        <div className="caller">
-          <h1>{name} is Calling...</h1>
-          <button onClick={answerCall}>Answer</button>
-        </div>
-        ) : null}
-      </div>
-    </div>
-  </div>
+    <form noValidate autoComplete="off">
+    <Paper>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <div className="video-container">
+            <div id="CallerVideo" className="video">
+              {stream && (
+                <video
+                  playsInline
+                  muted
+                  ref={myVideo}
+                  autoPlay
+                  style={{ width: "300px" }}
+                />
+              )}
+            </div>
+            <div id="RecieverVideo" className="video">
+              {callAccepted && !callEnded ? (
+                <video
+                  playsInline
+                  ref={userVideo}
+                  autoPlay
+                  style={{ width: "300px" }}
+                />
+              ) : null}
+            </div>
+          </div>
+          <div className="myId">
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Switch
+                  checked={turnCamera}
+                  onChange={(e) => checkTurnCamera(e)}
+                  name="checkedB"
+                  color="primary"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="txtName"
+                  label="Your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled
+                  id="filled-disabled"
+                  label="Your ID"
+                  value={me}
+                />
+                <CopyToClipboard text={me}>
+                  <Button variant="contained">Copy ID</Button>
+                </CopyToClipboard>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="txtCallTo"
+                  label="Call to ID"
+                  value={idToCall}
+                  onChange={(e) => setIdToCall(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <div className="call-button">
+                  {callAccepted && !callEnded ? (
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={leaveCall}
+                    >
+                      End Call
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      onClick={() => callUser(idToCall)}
+                      color="primary"
+                    >
+                      Call
+                    </Button>
+                  )}
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+          <div className="calling">
+            {receivingCall && !callAccepted ? (
+              <div className="caller">
+                <h1>{name} is Calling...</h1>
+                <Button
+                  variant="contained"
+                  onClick={answerCall}
+                  color="primary"
+                >
+                  Answer
+                </Button>
+              </div>
+            ) : null}
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+    </form>
   );
 }
 
